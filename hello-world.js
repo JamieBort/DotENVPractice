c6a7d5e8cfe33c38bb4651afa4af941cac48cd18
Jamie Bort <jamiebort@gmail.com>
2020-05-22 18:24:14 -0400

    Modified files.

--- hello-world.js
+++ hello-world.js
@@ -3,7 +3,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 require('dotenv').config();
-console.log(process.env);
+console.log(process.env); // The contents of the .env file.
+
 // const hello_world = "Hello World!";
 const hello_world = process.env.HELLO_WORLD; // HELLO_WORLD is coming from the .env file.
 
