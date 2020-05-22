a3eec208d21e77ae8f723cc96bdb5ee421e57b0c
Jamie Bort <jamiebort@gmail.com>
2020-05-22 17:13:24 -0400

    Got the dotENV module to work.

--- hello-world.js
+++ hello-world.js
@@ -2,11 +2,16 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+require('dotenv').config();
+console.log(process.env);
+// const hello_world = "Hello World!";
+const hello_world = process.env.HELLO_WORLD; // HELLO_WORLD is coming from the .env file.
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+//   res.end('Hello, World!\n');
+res.end(hello_world);
 });
 
 server.listen(port, hostname, () => {
